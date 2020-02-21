5ff2bf2037d4e4e9d63614b4bac44f4c0834a026
Edmund Larden <edmund@users.noreply.github.com>
2020-02-21 18:40:27 +0000

    Avoid $SAFE warnings from Ruby 2.7

--- lib/pry/exceptions.rb
+++ lib/pry/exceptions.rb
@@ -27,7 +27,7 @@ class Pry
   # Catches SecurityErrors if $SAFE is set
   module TooSafeException
     def self.===(exception)
-      $SAFE > 0 && exception.is_a?(SecurityError)
+      RUBY_VERSION < "2.7.0" && $SAFE > 0 && exception.is_a?(SecurityError)
     end
   end
 
